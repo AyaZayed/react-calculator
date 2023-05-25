8975f47f7e3ea2f7d751cab50e16e4781607199f
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-05-26 01:52:41 +0300

    hover state styles

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,24 +2,24 @@ import React from 'react'
 
 export default function Buttons({ calculate }) {
     const buttons = [
-        { id: 'seven', value: '7', class: 'button button-number' },
-        { id: 'eight', value: '8', class: 'button button-number' },
-        { id: 'nine', value: '9', class: 'button button-number' },
-        { id: 'del', value: 'DEL', class: 'button button-delete' },
-        { id: 'four', value: '4', class: 'button button-number' },
-        { id: 'five', value: '5', class: 'button button-number' },
-        { id: 'six', value: '6', class: 'button button-number' },
-        { id: 'add', value: '+', class: 'button button-operator' },
-        { id: 'one', value: '1', class: 'button button-number' },
-        { id: 'two', value: '2', class: 'button button-number' },
-        { id: 'three', value: '3', class: 'button button-number' },
-        { id: 'subtract', value: '-', class: 'button button-operator' },
-        { id: 'decimal', value: '.', class: 'button button-number' },
-        { id: 'zero', value: '0', class: 'button button-number' },
-        { id: 'divide', value: '/', class: 'button button-operator' },
-        { id: 'multiply', value: '*', class: 'button button-operator' },
-        { id: 'clear', value: 'RESET', class: 'button button-clear' },
-        { id: 'equals', value: '=', class: 'button button-operator' },
+        { id: 'seven', text: '7', value: '7', class: 'button button-number' },
+        { id: 'eight', text: '8', value: '8', class: 'button button-number' },
+        { id: 'nine', text: '9', value: '9', class: 'button button-number' },
+        { id: 'del', text: 'DEL', value: 'DEL', class: 'button button-delete' },
+        { id: 'four', text: '4', value: '4', class: 'button button-number' },
+        { id: 'five', text: '5', value: '5', class: 'button button-number' },
+        { id: 'six', text: '6', value: '6', class: 'button button-number' },
+        { id: 'add', text: '+', value: '+', class: 'button button-operator' },
+        { id: 'one', text: '1', value: '1', class: 'button button-number' },
+        { id: 'two', text: '2', value: '2', class: 'button button-number' },
+        { id: 'three', text: '3', value: '3', class: 'button button-number' },
+        { id: 'subtract', text: '-', value: '-', class: 'button button-operator' },
+        { id: 'decimal', text: '.', value: '.', class: 'button button-number' },
+        { id: 'zero', text: '0', value: '0', class: 'button button-number' },
+        { id: 'divide', text: '/', value: '/', class: 'button button-operator' },
+        { id: 'multiply', text: 'x', value: '*', class: 'button button-operator' },
+        { id: 'clear', text: 'RESET', value: 'RESET', class: 'button button-clear' },
+        { id: 'equals', text: '=', value: '=', class: 'button button-operator' },
     ]
     return (
         <>
@@ -35,7 +35,7 @@ export default function Buttons({ calculate }) {
                                 calculate(e)
                             }}
                         >
-                            {button.value}
+                            {button.text}
                         </button>
                     )
                 })
